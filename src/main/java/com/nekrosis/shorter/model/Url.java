119e0d5808218d74716e429fffbd0ae03814ca0a
Nekrosis <ryuk.nekro@gmail.com>
2023-02-20 04:21:04 +0300

    second commit

--- src/main/java/com/nekrosis/shorter/model/Url.java
+++ src/main/java/com/nekrosis/shorter/model/Url.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 
 @Entity
 @Table(name = "url")
